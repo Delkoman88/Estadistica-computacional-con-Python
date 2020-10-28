752de7369ff21491b5c98667978bbc45a79439be
Edgar Mancilla <delkoman@gmail.com>
2020-10-28 13:42:11 -0700

    ejemplo de simulacion de caminos aleatorios

--- Camino aleatorio/borracho.py
+++ Camino aleatorio/borracho.py
@@ -11,5 +11,5 @@ class BorrachoTradicional(Borracho):
     def __init__(self, nombre):
         super().__init__(nombre)
 
-        def camina():
-            return random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
+    def camina(self):
+        return random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
